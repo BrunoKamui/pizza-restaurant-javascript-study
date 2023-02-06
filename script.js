9ec7ec6bf3fe731da6bde08598ce892f3c96a046
Bruno Kamui <brunokamui.work@gmail.com>
2023-02-06 06:59:56 -0300

    feat: add all products information on screen

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const getAll = (el) => document.querySelectorAll(el);
 
 pizzaJson.map((item, index) => {
   let pizzaItem = getEl('.models .pizza-item').cloneNode(true);
+
+  pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+  pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2).replace('.', ',')}`;
+  pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+  pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
   
   getEl('.pizza-area').append(pizzaItem);
 })
