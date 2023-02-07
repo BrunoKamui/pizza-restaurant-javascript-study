83cec198aaa647b9b51ee2a3491a70efe61d3378
Bruno Kamui <brunokamui.work@gmail.com>
2023-02-07 17:40:32 -0300

    feat: add modal product name, img and info

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const getAll = (el) => document.querySelectorAll(el);
 pizzaJson.map((item, index) => {
   let pizzaItem = getEl('.models .pizza-item').cloneNode(true);
 
+  pizzaItem.querySelector('a').setAttribute('data-key', index);
   pizzaItem.querySelector('.pizza-item--img img').src = item.img;
   pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2).replace('.', ',')}`;
   pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
@@ -11,6 +12,11 @@ pizzaJson.map((item, index) => {
 
   pizzaItem.querySelector('a').addEventListener('click', (e) => {
     e.preventDefault();
+    let key = e.currentTarget.getAttribute('data-key');
+
+    getEl('.pizzaBig img').src = pizzaJson[key].img;
+    getEl('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+    getEl('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
 
     getEl('.pizzaWindowArea').style.opacity = 0;
     getEl('.pizzaWindowArea').style.display = 'flex';
