da8bff2e5df551f34d358165d18fba29575493c4
Bruno Kamui <brunokamui.work@gmail.com>
2023-02-08 12:08:07 -0300

    feat: add close modal and modal counter

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const getEl = (el) => document.querySelector(el);
 const getAll = (el) => document.querySelectorAll(el);
 
+let modalCount = 1
+
+//Pizza listing
 pizzaJson.map((item, index) => {
   let pizzaItem = getEl('.models .pizza-item').cloneNode(true);
 
@@ -13,6 +16,7 @@ pizzaJson.map((item, index) => {
   pizzaItem.querySelector('a').addEventListener('click', (e) => {
     e.preventDefault();
     let key = e.currentTarget.getAttribute('data-key');
+    modalCount = 1
 
     getEl('.pizzaBig img').src = pizzaJson[key].img;
     getEl('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
@@ -26,6 +30,8 @@ pizzaJson.map((item, index) => {
       size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
     });
 
+    getEl('.pizzaInfo--qt').innerHTML = modalCount;
+
     getEl('.pizzaWindowArea').style.opacity = 0;
     getEl('.pizzaWindowArea').style.display = 'flex';
     setTimeout(() => {
@@ -34,4 +40,28 @@ pizzaJson.map((item, index) => {
   });
   
   getEl('.pizza-area').append(pizzaItem);
+});
+
+// Modal Events
+function closeModal() {
+  getEl('.pizzaWindowArea').style.opacity = 0;
+  setTimeout(() => {
+    getEl('.pizzaWindowArea').style.display = 'none';
+  }, 500);
+};
+
+getAll('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((button) => {
+  button.addEventListener('click', closeModal);
+});
+
+getEl('.pizzaInfo--qtmais').addEventListener('click', () => {
+  modalCount += 1;
+  getEl('.pizzaInfo--qt').innerHTML = modalCount;
 })
+
+getEl('.pizzaInfo--qtmenos').addEventListener('click', () => {
+  if(modalCount > 1) {
+    modalCount -= 1;
+    getEl('.pizzaInfo--qt').innerHTML = modalCount;
+  }
+});
